3668b344f071d24e82e24ad5f382f6f0f2f9d47f
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-09-11 11:35:39 -0400

    Update server.js

--- src/services/server.js
+++ src/services/server.js
@@ -1,7 +1,5 @@
 // Load the http module to create an HTTP server.
 const http = require('http');
-
-// Configure our HTTP server to respond with "Hello, Node!" to all requests.
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, Node!');
